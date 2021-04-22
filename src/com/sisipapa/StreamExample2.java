a90e8b4b5b528e65227cf25711a6066b4ecdc059
KKH <coolguy239@naver.com>
2021-04-23 00:37:43 +0900

    Stream filter,sort,map,flatmap 정리

--- src/com/sisipapa/StreamExample2.java
+++ src/com/sisipapa/StreamExample2.java
@@ -1,11 +1,59 @@
 package com.sisipapa;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample2 {
 
     private static final List<Integer> NUMBERS = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    public static void basic1(){
+        // 하나의 filter를 사용
+        IntStream intStream1 = IntStream.rangeClosed(1, 10);
+        intStream1.filter(i -> i%2!=0 && i%3!=0).forEach(System.out::println);
+
+        // 두개의 filter를 사용
+        IntStream intStream2 = IntStream.rangeClosed(1, 10);
+        intStream2.filter(i -> i%2!=0).filter(i -> i%3!=0).forEach(System.out::println);
+    }
+
+    public static void basic2(){
+        System.out.println("###기본정렬");
+        Stream<String> strStream1 = Stream.of("FFFFFFF", "ccc", "A", "BB", "eeeee", "dddd");
+        strStream1.sorted().forEach(System.out::print);
+        System.out.println("\n===============================================================================");
+
+        System.out.println("###기본정렬 역순");
+        Stream<String> strStream2 = Stream.of("FFFFFFF", "ccc", "A", "BB", "eeeee", "dddd");
+        strStream2.sorted(Comparator.reverseOrder()).forEach(System.out::print);
+        System.out.println("\n===============================================================================");
+
+        System.out.println("###대소문자 구분없이");
+        Stream<String> strStream3 = Stream.of("FFFFFFF", "ccc", "A", "BB", "eeeee", "dddd");
+        strStream3.sorted(String.CASE_INSENSITIVE_ORDER).forEach(System.out::print);
+        System.out.println("\n===============================================================================");
+
+        System.out.println("###길이 정렬");
+        Stream<String> strStream4 = Stream.of("FFFFFFF", "ccc", "A", "BB", "eeeee", "dddd");
+        strStream4.sorted(Comparator.comparing(String::length)).forEach(System.out::print);
+        System.out.println("\n===============================================================================");
+    }
+
+    public static void basic3(){
+        List<String> list = List.of("a1", "a2", "b1", "b2", "c2", "c1", "c3");
+        Stream<String> stream = list.stream();
+        stream.map(String::toUpperCase).forEach(System.out::println);
+    }
+
+    public static void basic4(){
+        String[][] arrays = new String[][]{ {"aa1", "aa2"}, {"bb1", "bb2"}, {"cc1", "cc2", "cc3"} };
+        Stream<String[]> stream1 = Arrays.stream(arrays);
+        Stream<String> stream2 = stream1.flatMap(s -> Arrays.stream(s));
+        stream2.forEach(System.out::println);
+    }
 
     /**
      * 반복문/제어문을 활용
@@ -65,6 +113,11 @@ public class StreamExample2 {
     }
 
     public static void main(String[] args) {
+        basic1(); // filter
+        basic2(); // sort
+        basic3(); // map
+        basic4(); // flatMap
+
         method1();
         method2();
         method3();
